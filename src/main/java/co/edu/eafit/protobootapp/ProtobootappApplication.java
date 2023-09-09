98d44411c3e71b228cbbc7db5665fcd976c4c264
felipealvarezb <felipealvarezb1@gmail.com>
2023-09-09 08:23:00 -0500

    Second Commit

--- src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
+++ src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
@@ -19,7 +19,7 @@ public class ProtobootappApplication {
     }
 
     //Se ejecuta con http://localhost:8080/
-    @GetMapping("/")
+    @GetMapping("/home")
     public String index() {
         return String.format("Bienvenido! Esta aplicación ayuda a elevar números a sus potencias y a entender AWS.");
     }    
